0e6b123a522cb4fc76da10648d26d8ccb8f4dd7f
Yogesh Saroya <yogeshsaroya@gmail.com>
2022-07-02 21:43:12 +0530

    fix

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -102,6 +102,7 @@ class UsersController extends AppController
                         }
                     }else{ echo '<div class="alert alert-danger">Please select file.</div>'; }
                 } catch (\Throwable $th) {
+                    pr($th);
                     echo '<div class="alert alert-danger">Please try again.</div>';
                 }
             }
